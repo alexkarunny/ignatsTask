e3e63fcd7d6d5e7bc3ad5bd35d3bdf12cada97d0
alexkarunny <smetan4ik1@gmail.com>
2021-11-10 16:58:57 +0300

    HW9, date, time

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,34 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import classes from './clock.module.css'
+
+//const get2Digits = (num: number) => num < 10 ? '0' + num : num
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString() //`${get2Digits(date.getHours())}:${get2Digits(date.getMinutes())}:${get2Digits(date.getSeconds())}`
+    const stringDate = date.toLocaleDateString() // fix with date
 
     return (
         <div>
@@ -35,12 +38,13 @@ function Clock() {
             >
                 {stringTime}
             </div>
-
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            <div className={classes.date}>
+                {show && (
+                    <div>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
